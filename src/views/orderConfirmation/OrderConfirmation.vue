7f9277c4cc175c7c74987b3f50b15524cb1cbbf2
waynewang98 <1070546617@qq.com>
2021-06-07 21:02:44 +0800

    结算页面底部布局和bug修复

--- src/views/orderConfirmation/OrderConfirmation.vue
+++ src/views/orderConfirmation/OrderConfirmation.vue
@@ -2,7 +2,9 @@
   <div class="wrapper">
     <div class="top">
       <div class="top__header">
-        <div class="iconfont top__header__back">&#xe677;</div>
+        <div class="iconfont top__header__back"
+        @click="handleBackClick"
+      >&#xe677;</div>
         确认订单
       </div>
       <div class="top__receiver">
@@ -18,41 +20,50 @@
     <div class="products">
       <div class="products__title">{{shopName}}</div>
       <div class="products__list">
-        <div
-          class="products__item"
+        <template
           v-for="item in productList"
           :key="item._id"
         >
-          <img class="products__item__img" :src="item.imgUrl" alt="">
-          <div class="products__item__detail">
-            <h4 class="products__item__title">{{item.name}}</h4>
-            <p class="products__item__price">
-              <span>
-                <span class="products__item__yen">&yen;</span>
-                {{item.price}} x {{item.count}}
-              </span>
-              <span class="products__item__total">
-                <span class="products__item__yen">&yen;</span>
-                {{item.price * item.count}}
-              </span>
-            </p>
-          </div>
+          <div class="products__item" v-if="item.count > 0">
+            <img class="products__item__img" :src="item.imgUrl" alt="">
+            <div class="products__item__detail">
+              <h4 class="products__item__title">{{item.name}}</h4>
+              <p class="products__item__price">
+                <span>
+                  <span class="products__item__yen">&yen;</span>
+                  {{item.price}} x {{item.count}}
+                </span>
+                <span class="products__item__total">
+                  <span class="products__item__yen">&yen;</span>
+                  {{item.price * item.count}}
+                </span>
+              </p>
+            </div>
         </div>
+        </template>
       </div>
+    </div>
+    <div class="order">
+      <div class="order__price">实付金额<b> &yen;{{calculations.price}}</b></div>
+      <div class="order__btn">提交订单</div>
     </div>
   </div>
 </template>
 
 <script>
-import { useRoute } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { useCommonCartEffect } from '../../effects/cartEffects'
 export default {
   name: 'OrderConfirmation',
   setup () {
     const route = useRoute()
+    const router = useRouter()
     const shopId = route.params.id
-    const { productList, shopName } = useCommonCartEffect(shopId)
-    return { productList, shopName }
+    const { productList, shopName, calculations } = useCommonCartEffect(shopId)
+    const handleBackClick = () => {
+      router.back()
+    }
+    return { productList, shopName, calculations, handleBackClick }
   }
 }
 </script>
@@ -67,6 +78,7 @@ export default {
   top: 0;
   bottom: 0;
   background-color: #eee;
+  overflow-y: scroll;
 }
 .top {
   position: relative;
@@ -127,7 +139,7 @@ export default {
   }
 }
 .products {
-  margin: .16rem .18rem .55rem .18rem;
+  margin: .16rem .18rem .1rem .18rem;
   background: #fff;
   &__title {
     padding: .16rem .16rem 0 .16rem;
@@ -145,6 +157,14 @@ export default {
     }
     &__detail {
       flex: 1;
+    }
+    &__list {
+      overflow-y: scroll;
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: .6rem;
+      top: 2.4rem;
     }
     &__title {
       margin: 0;
@@ -168,6 +188,29 @@ export default {
     &__yen {
       font-size: .12rem;
     }
+  }
+}
+.order {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  height: .49rem;
+  line-height: .49rem;
+  background: #fff;
+  &__price {
+    flex: 1;
+    text-indent: .24rem;
+    font-size: .14rem;
+    color: #333;
+  }
+  &__btn {
+    width: .98rem;
+    background: #4FB0F9;
+    color: #fff;
+    text-align: center;
+    font-size: .14rem;
   }
 }
 </style>
